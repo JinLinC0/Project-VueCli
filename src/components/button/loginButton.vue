f09505b344ed8255352655c658663d0de3f1442b
jlc <2794810071@qq.com>
2025-03-30 18:12:19 +0800

    update：全局组件的动态注册

---
+++ src/components/button/loginButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button>zhucbutton</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,6 @@
-import { App } from "vue";
+import { App, defineAsyncComponent } from "vue";
 import { setupTailwindcss } from "./tailwindcss";
+import _ from 'lodash'
 
 /***
  * @description 设置插件的方法
@@ -7,4 +8,18 @@ import { setupTailwindcss } from "./tailwindcss";
  */
 export function setupPlugins(app: App) {
     setupTailwindcss()
+    autoRegisterComponent(app)
+}
+
+// 自动注册全局组件
+function autoRegisterComponent(app: App) {
+    // 加载components文件夹下的所有组件
+    const components = import.meta.glob('../components/**/*.vue')
+    console.log(components)
+    // 遍历components文件夹，提取组件的文件名，建议组件文件名也使用驼峰命名，如loginButton.vue
+    Object.keys(components).forEach((key) => {
+        const componentName = key.split('/').pop()?.split('.').shift() as string
+        // 注册组件   _.camelCase(componentName) 将文件名转换为驼峰命名，组件调用时使用驼峰命名的方式进行调用
+        app.component(_.camelCase(componentName), defineAsyncComponent(components[key] as () => Promise<{ default: typeof import('vue') }>))
+    })
 }
