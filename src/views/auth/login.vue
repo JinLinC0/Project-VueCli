a782239df4c6f030895ae0a7f4a6bfc49f357ee8
jlc <2794810071@qq.com>
2025-03-30 15:33:11 +0800

    update：登录界面的完善

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="bg-slate-400 h-screen flex justify-center items-center">
-        <div class="w-[720px] bg-slate-100 -translate-y-16 grid grid-cols-2 rounded-md shadow-2xl overflow-hidden">
-            <div>
-                <img src="/images/login.jpg" class="h-96 w-full object-cover" />
+    <div class="bg-slate-400 h-screen flex justify-center md:items-center p-5">
+        <div class="w-[720px] bg-slate-100 md:-translate-y-16 md:grid grid-cols-2 rounded-md shadow-2xl overflow-hidden">
+            <div class="hidden md:block">
+                <img src="/images/login.jpg" class="h-80 w-full object-cover" />
             </div>
             <div class="p-6">
                 <h2 class="text-center text-gray-600 text-lg">用户登录</h2>
-                <div>
-                    <input 
-                        type="text" 
-                        class="w-full border border-gray-300 rounded-md p-2 mt-4" 
-                        placeholder="请输入用户名"
-                    />
+                <div class="mt-5">
+                    <input type="text" placeholder="请输入用户名" class="login-input" />
+                    <input type="text" placeholder="请输入密码" class="login-input mt-5" />
+                </div>
+                <button class="login-button mt-8">登录</button>
+                <div class="flex gap-2 justify-center mt-5">
+                    <a href class="text-xs text-gray-700 hover:text-blue-300">找回密码</a>
+                    <a href class="text-xs text-gray-700 hover:text-blue-300">配套笔记</a>
                 </div>
             </div>
         </div>
@@ -22,4 +24,12 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.login-input {
+    @apply w-full border border-gray-300 rounded-md p-2 mt-4 outline-none
+    placeholder:text-xs focus:border-blue-500 duration-300
+}
+.login-button {
+    @apply bg-blue-500 text-white w-full rounded-md py-2 hover:bg-blue-600 duration-300
+}
+</style>
