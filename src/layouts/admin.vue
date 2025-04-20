2ba3cf067d4346d8c2a0549527315fdd88bfcac8
jlc <2794810071@qq.com>
2025-04-20 20:43:09 +0800

    update：历史菜单栏的添加

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="admin h-screen w-screen flex">
+        <!-- 左侧菜单栏 -->
         <MenuComponet />
         <!-- 右侧主要内容 -->
         <div class="content flex-1 bg-gray-200">
+            <!-- 顶部导航栏 -->
             <NavbarComponet />
+            <!-- 历史记录链接栏 -->
+            <HistoryLinkComponet />
             <router-view />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import HistoryLinkComponet from './admin/history_link.vue';
 import MenuComponet from './admin/menu.vue'
 import NavbarComponet from './admin/navbar.vue'
 </script>
---
+++ src/layouts/admin/history_link.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="m-3 grid grid-flow-col gap-2 justify-start">
+        <a v-for="link of links" class="bg-white rounded-s-md py-2 px-3 flex items-center justify-center">
+            <span class="text-sm text-gray-600">{{ link.title }}</span>
+            <el-icon class="ml-1"><Close /></el-icon>
+        </a>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const links = ref<{ title: string }[]> ([
+    {title: '编辑器'},
+    {title: '图表'},
+    {title: '按钮'},
+    {title: '文本块'},
+    {title: '提示信息'},
+    {title: '菜单'},
+])
+</script>
+
+<style scoped></style>
