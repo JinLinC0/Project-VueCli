65817396b3f3071782568936c8e94d6e984baee9
jlc <2794810071@qq.com>
2025-04-22 21:08:38 +0800

    update：登录注册路由不加载的问题修复

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,12 @@ const routes = [
         path: "/",
         name: "home",
         component: () => import("@/views/home.vue")
+    },
+    {
+        // 登录注册路由
+        path: "/login",
+        name: "login",
+        component: () => import("@/views/auth/login.vue")
     }
 ] as RouteRecordRaw[]   // 声明类型，用于存放路由的数组
 
