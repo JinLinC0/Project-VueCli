d33670009a59be2820c9173caed8c48a0ae39918
jlc <2794810071@qq.com>
2025-05-10 13:28:44 +0800

    fix：打包问题的修复

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -2,7 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import { Plugin } from 'vite'
 import { setupMockPlugin } from './mock'
 
-export function setupPlugins(isBuild: boolean, _env: ViteEnv) {
+export function setupPlugins(isBuild: boolean, _env: importMetaEnv) {
     // 返回原先就存在的vue()插件
     const plugins: Plugin[] = [vue()]
     // 添加mockjs的配置
